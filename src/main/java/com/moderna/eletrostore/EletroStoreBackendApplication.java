da249b2340f807420563c58a7c988ad55e9e4e52
pedrolima <pedro.lima@picpay.com>
2023-05-13 16:32:10 -0300

    primeiro commit

---
+++ src/main/java/com/moderna/eletrostore/EletroStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moderna.eletrostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moderna/eletrostore/EletroStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moderna.eletrostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletroStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
